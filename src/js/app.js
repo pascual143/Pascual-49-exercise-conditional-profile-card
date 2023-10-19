0836c899e2872569b70f3f1e3ffe20b18be5e6d5
Pascual Vila <46532433+pascual143@users.noreply.github.com>
2023-10-19 09:49:02 +0000

    auit default 4geeks links

--- src/js/app.js
+++ src/js/app.js
@@ -45,16 +45,16 @@ function render(variables = {}) {
   }
           </h3>        
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/4geeksacademy/${
+            <li><a href="https://twitter.com/${
               variables.twitter
             }"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy/${
+            <li><a href="https://github.com/${
               variables.github
             }"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy/${
+            <li><a href="https://linkedin.com/school/${
               variables.linkedin
             }"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy/${
+            <li><a href="https://instagram.com/${
               variables.instagram
             }"><i class="fab fa-instagram"></i></a></li>
           </ul>
